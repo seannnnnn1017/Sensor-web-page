d9d74ed1a01ff82b007cfa357d86810600dbf296
Lin Chen Xian <113087463+xain0411@users.noreply.github.com>
2025-03-06 14:38:53 +0800

    Update temperture_main.py

--- temperture_main.py
+++ temperture_main.py
@@ -1,8 +1,17 @@
-from temp_py_package import read_temperature, continuous_read
+import threading
+import sys
+from temp_py_package import continuous_read
+
+def shutdown():
+    print("五分鐘已到，自動關閉程式。")
+    sys.exit(0)
 
 def main():
     port = 'COM3'
+    timer = threading.Timer(300, shutdown)
+    timer.start()
     continuous_read(port)
+    timer.cancel()
 
 if __name__ == '__main__':
     main()
